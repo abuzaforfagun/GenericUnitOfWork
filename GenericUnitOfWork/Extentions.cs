24f2169cacef25e45ae9cb9d46103f21c469c135
Fagun <mdabuzaforfagun@gmail.com>
2020-04-19 17:07:08 +0600

    Add extention methods

---
+++ GenericUnitOfWork/Extentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GenericUnitOfWork
+{
+    public static class Extentions
+    {
+        public static void AddUnitOfWork<Context>(this IServiceCollection services)
+            where Context : DbContext
+        {
+            services.AddTransient(typeof(EntityRepository<,>), typeof(EntityRepository<,>));
+            services.AddScoped<IUnitOfWork, UnitOfWork<Context>>();
+        }
+    }
+}
